69aff86ef82a51607a30511a578c15aefa4e5134
Hariprasath Muthumanickam <39271101+mkhariprasath@users.noreply.github.com>
2022-04-06 22:12:16 +0530

    Update test_Sender.cpp

--- test_Sender.cpp
+++ test_Sender.cpp
@@ -19,3 +19,10 @@ TEST_CASE("Check the data is printed on console") {
     REQUIRE(fetchData (inputMethod) == E_OK);
     REQUIRE(passDataToOutput (outputMethod) == E_OK);
 }
+
+
+TEST_CASE("Test the main fuction of the sender ") {
+    dataFetchChannel inputMethod = viaFile;
+    dataOutputChannel outputMethod = OutputChannelTest;
+    REQUIRE(senderMain(inputMethod, outputMethod) == E_TEST_OK);
+}

8770b6f5182e46be22f022f2a90b4a0f8988566a
Hariprasath Muthumanickam <39271101+mkhariprasath@users.noreply.github.com>
2022-04-07 00:18:53 +0530

    Update test_Sender.cpp

--- test_Sender.cpp
+++ test_Sender.cpp
@@ -14,7 +14,7 @@ TEST_CASE("Get the data temperature and soc data from file which doesn't exist")
     char fname[] = "./InputDummy.txt";
     strcpy(fileName, fname);
     Status_t (*fp_InputFunction)() = readDataFromFile;
-    REQUIRE(fetchData (fp_InputFunction) == E_OK);
+    REQUIRE(fetchData (fp_InputFunction) == E_NOT_OK);
 }
 
 

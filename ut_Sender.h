8166e6d997fa97daf871d5b975907f6f0b2b75a9
Hariprasath Muthumanickam <39271101+mkhariprasath@users.noreply.github.com>
2022-04-06 23:45:37 +0530

    Create ut_Sender.h

---
+++ ut_Sender.h
@@ -0,0 +1,10 @@
+#ifndef UT_SENDER_H
+#define UT_SENDER_H
+
+#include "sender.h"
+
+Status_t testWithNoInputFile();
+Status_t testOutput();
+Status_t fillRandomData();
+
+#endif // multiple include protection for UT_SENDER_H

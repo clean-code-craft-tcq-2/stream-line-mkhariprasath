8424aaf287ac23f2adcb7452f8de9e0251f25106
KiruthighaKMuthusamy <91048874+KiruthighaKMuthusamy@users.noreply.github.com>
2022-04-26 13:49:01 +0530

    Update receiver.h

--- receiver.h
+++ receiver.h
@@ -8,3 +8,12 @@
 float Temp[NOOFDATA] = {};
 float SOC[NOOFDATA] = {};
 float sensorsID[NOOFDATA] = {};
+
+
+
+Status_t GetFromConsole();
+float findMaxValue(float * input,int noOfElements);
+float findMinValue(float * input,int noOfElements);
+float aveOfLastConsecutiveValues(float * input,int noOfElements);
+void printOnConsole(float* Max ,float* Min, float* Ave);
+void receiverMainFunction(void (*fpPrintOnConsole)(float* ,float*, float*));

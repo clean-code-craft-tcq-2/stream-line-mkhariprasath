32ec8d9957ce03fe972dc2243555d13d93182bf4
KiruthighaKMuthusamy <91048874+KiruthighaKMuthusamy@users.noreply.github.com>
2022-04-26 13:25:28 +0530

    Update receiver.h

--- receiver.h
+++ receiver.h
@@ -4,15 +4,6 @@
 
 #define NO_OF_CONSECUTIVE_AVERAGE 5
 
-#define NOOFDATA 50
-
-typedef enum
-{
-	E_NOT_OK,
-	E_OK,
-    E_TEST_OK
-}Status_t;
-
 float Temp[NOOFDATA] = {};
 float SOC[NOOFDATA] = {};
-float   sensorsID[NOOFDATA] = {};
+float sensorsID[NOOFDATA] = {};

b544394ef908a8b9167547482c4f9a5b4b702033
KiruthighaKMuthusamy <91048874+KiruthighaKMuthusamy@users.noreply.github.com>
2022-04-26 15:21:31 +0530

    Update receiver.h

--- receiver.h
+++ receiver.h
@@ -2,9 +2,9 @@
 
 #define NO_OF_CONSECUTIVE_AVERAGE 5
 
-float Temp[NOOFDATA] = {};
-float SOC[NOOFDATA] = {};
-float sensorsID[NOOFDATA] = {};
+float Temp[NOOFDATA]; = 
+float SOC[NOOFDATA] ;= 
+float sensorsID[NOOFDATA]; 
 
 
 

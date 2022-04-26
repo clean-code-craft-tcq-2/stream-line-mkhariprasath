b56698b8ee91ac45eefca851aabef79a4ddbe341
KiruthighaKMuthusamy <91048874+KiruthighaKMuthusamy@users.noreply.github.com>
2022-04-26 16:10:01 +0530

    Add files via upload

---
+++ receiver.h
@@ -0,0 +1,16 @@
+#include "sender.h"
+
+#define NO_OF_CONSECUTIVE_AVERAGE 5
+
+extern float Temp[NOOFDATA]; 
+extern float SOC[NOOFDATA] ;
+extern float sensorsID[NOOFDATA]; 
+
+
+
+Status_t GetFromConsole();
+float findMaxValue(float * input,int noOfElements);
+float findMinValue(float * input,int noOfElements);
+float aveOfLastConsecutiveValues(float * input,int noOfElements);
+void printOnConsole(float* Max ,float* Min, float* Ave);
+void receiverMainFunction(void (*fpPrintOnConsole)(float* ,float*, float*));

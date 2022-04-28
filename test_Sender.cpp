03e525563e6cd462c6470f79c1a6c650a7afde7e
KiruthighaKMuthusamy <91048874+KiruthighaKMuthusamy@users.noreply.github.com>
2022-04-28 21:35:33 +0530

    Update test_Sender.cpp

--- test_Sender.cpp
+++ test_Sender.cpp
@@ -15,13 +15,6 @@ TEST_CASE("Get the data temperature and soc data from random number generator") 
     REQUIRE(fetchData (fp_InputFunction) == E_OK);
 }
 
-TEST_CASE("Check the data is printed on console") {
-    Status_t (*fp_InputFunction)() = readDataFromFile;
-    Status_t (*fp_OutputFunction)() = passToConsole;
-    REQUIRE(fetchData (fp_InputFunction) == E_OK);
-    REQUIRE(passDataToOutput (fp_OutputFunction) == E_OK);
-}
-
 TEST_CASE("Test the main fuction of the sender without input file") {
     Status_t (*fp_InputFunction)() = testWithNoInputFile;
     Status_t (*fp_OutputFunction)() = testOutput;
@@ -34,4 +27,11 @@ TEST_CASE("Test the main fuction of the sender ") {
     Status_t (*fp_OutputFunction)() = testOutput;
 
     REQUIRE(senderMain(fp_InputFunction, fp_OutputFunction) == E_TEST_OK);
+}
+
+TEST_CASE("Check the data is printed on console") {
+    Status_t (*fp_InputFunction)() = readDataFromFile;
+    Status_t (*fp_OutputFunction)() = passToConsole;
+    REQUIRE(fetchData (fp_InputFunction) == E_OK);
+    REQUIRE(passDataToOutput (fp_OutputFunction) == E_OK);
 }

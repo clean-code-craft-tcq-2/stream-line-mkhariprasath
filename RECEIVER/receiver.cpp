8ae2332b213961444a9eaf9233188dd3ab6cbda6
KiruthighaKMuthusamy <91048874+KiruthighaKMuthusamy@users.noreply.github.com>
2022-04-28 16:30:35 +0530

    Update receiver.cpp

--- RECEIVER/receiver.cpp
+++ RECEIVER/receiver.cpp
@@ -29,3 +29,12 @@ TEST_CASE("Checks average value finding function")
         REQUIRE(actualOutput==expectedOutput);
 }
 
+TEST_CASE("Get the data from sensor by sender and pass the data to receiver") 
+{
+    Status_t (*fp_InputFunction)() = readDataFromFile;
+    Status_t (*fp_OutputFunction)() = passToConsole;
+    void receiverMainFunction(&printOnConsole);
+    REQUIRE(fetchData (fp_InputFunction) == E_OK);
+    REQUIRE(passDataToOutput (fp_OutputFunction) == E_OK);
+ //REQUIRE(passDataToOutput (fp_OutputFunction) == E_OK);
+}

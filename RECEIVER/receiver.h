1b8f4e81ffad09b3dafb1ca00e05a1b5bcf1144d
KiruthighaKMuthusamy <91048874+KiruthighaKMuthusamy@users.noreply.github.com>
2022-04-28 21:32:43 +0530

    Update receiver.h

--- RECEIVER/receiver.h
+++ RECEIVER/receiver.h
@@ -8,3 +8,13 @@ float findMinValue(float * input,int noOfElements);
 float aveOfLastConsecutiveValues(float * input,int noOfElements);
 void printOnConsole(float* Max ,float* Min, float* Ave);
 void receiverMainFunction(void (*fpPrintOnConsole)(float* ,float*, float*));
+
+
+
+extern Status_t readDataFromFile();
+extern Status_t fillRandomData();
+extern Status_t passToConsole();
+extern float    optimumValuesRandom(float min, float max);
+
+extern Status_t fetchData(Status_t (*fp_InputFunction)());
+extern Status_t passDataToOutput(Status_t (*fp_OutputFunction)());

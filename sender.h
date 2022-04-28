3d9d3e29023da6c0e947c23e6e9e148393941aa8
KiruthighaKMuthusamy <91048874+KiruthighaKMuthusamy@users.noreply.github.com>
2022-04-28 12:00:15 +0530

    Update sender.h

--- sender.h
+++ sender.h
@@ -25,9 +25,9 @@ typedef enum
     E_TEST_OK
 }Status_t;
 
- float  Temperature  [NOOFDATA] ;
- float StateOfCharge[NOOFDATA];
- int   sensorID     [NOOFDATA] ;
+ extern float  Temperature  [NOOFDATA] ;
+extern  float StateOfCharge[NOOFDATA];
+ extern int   sensorID     [NOOFDATA] ;
 
 
 Status_t readDataFromFile();

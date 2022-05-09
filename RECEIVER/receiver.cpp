e47cdab34e02abf9d4dd2d35280010cad6d3bff9
KiruthighaKMuthusamy <91048874+KiruthighaKMuthusamy@users.noreply.github.com>
2022-05-09 11:23:28 +0530

    Update receiver.cpp

--- RECEIVER/receiver.cpp
+++ RECEIVER/receiver.cpp
@@ -6,26 +6,26 @@
 
 TEST_CASE("Checks maximum value finding function")
  {
-	float TemperatureInput[]= {2.5,10,14,22.5,47,14.5,22,0.5,2.5};
-	float expectedOutput  = 47;
-	float actualOutput  = findMaxValue( TemperatureInput,9);
+	int TemperatureInput[]= {2.5,10,14,22.5,47,14.5,22,0.5,2.5};
+	int expectedOutput  = 47;
+	int actualOutput  = findMaxValue( TemperatureInput,9);
         REQUIRE(actualOutput==expectedOutput);
 }
 
 
 TEST_CASE("Checks Minimum value finding function")
  {
-	float TemperatureInput[]= {2.5,10,14,22.5,47,14.5,22,0.5,2.5};
-	float expectedOutput  = 0.5;
-	float actualOutput  = findMinValue( TemperatureInput,9);
+	int TemperatureInput[]= {2.5,10,14,22.5,47,14.5,22,1.5,2.5};
+	int expectedOutput  = 1;
+	int actualOutput  = findMinValue( TemperatureInput,9);
        REQUIRE(actualOutput==expectedOutput);
 }
 
 TEST_CASE("Checks average value finding function")
  {
-	float TemperatureInput[]= {2.5,10,14,22.5,47,14.5,22,0.5,2.5};
-	float expectedOutput  = 17.299999;
-	float actualOutput  = aveOfLastConsecutiveValues(TemperatureInput,9);
+	int TemperatureInput[]= {2.5,10,14,22.5,47,14.5,22,0.5,2.5};
+	int expectedOutput  = 17;
+	int actualOutput  = aveOfLastConsecutiveValues(TemperatureInput,9);
         REQUIRE(actualOutput==expectedOutput);
 }
 

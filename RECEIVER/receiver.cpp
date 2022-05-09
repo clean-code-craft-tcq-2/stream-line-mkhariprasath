19edd9b0edcb5aeeeb479dd26f3db6572018a3c4
KiruthighaKMuthusamy <91048874+KiruthighaKMuthusamy@users.noreply.github.com>
2022-05-09 11:45:46 +0530

    Update receiver.cpp

--- RECEIVER/receiver.cpp
+++ RECEIVER/receiver.cpp
@@ -3,6 +3,22 @@
 #include"../test/catch.hpp"
 #include"receiver.h"
 
+TEST_CASE("Check Sensor ID is taken properly from the console data")
+ {
+	int sensorID[50];
+	char consoleData[5][20] = ( 1, 20, 40
+				   2, 22, 42
+				   4, 33,43
+				   3,44,54
+				   5,55,65);
+	collectIdData( consoleData, sensorID, 5);
+				  
+	   REQUIRE(sensorID[0] == 1);
+            REQUIRE(sensorID[1] == 2);
+                REQUIRE(sensorID[2] == 4);
+                REQUIRE(sensorID[3] == 3);
+	REQUIRE(sensorID[4] == 5);
+}
 
 TEST_CASE("Checks maximum value finding function")
  {

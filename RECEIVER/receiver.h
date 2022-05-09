0ac13995411c14eef481cef57e0d69918053bd0e
KiruthighaKMuthusamy <91048874+KiruthighaKMuthusamy@users.noreply.github.com>
2022-05-09 11:26:24 +0530

    Update receiver.h

--- RECEIVER/receiver.h
+++ RECEIVER/receiver.h
@@ -8,8 +8,8 @@
 void GetFromConsole();
 void collectIdData(char consoleData[50][20], int ampData[50], int size);
 void collectTempData(char input[50][20], int tempData[50], int size);
-float findMaxValue(int * input,int noOfElements);
-float findMinValue(int * input,int noOfElements);
-float aveOfLastConsecutiveValues(int * input,int noOfElements);
-void printOnConsole(float* Max ,int* Min, int* Ave);
+int findMaxValue(int * input,int noOfElements);
+int findMinValue(int * input,int noOfElements);
+int aveOfLastConsecutiveValues(int * input,int noOfElements);
+void printOnConsole(int* Max ,int* Min, int* Ave);
 void receiverMainFunction(void (*fpPrintOnConsole)(int* ,int*, int*));
